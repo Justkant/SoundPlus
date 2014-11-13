362e9e9d470b802694ba11d9eac4b569e1a9091d
MetaCorp <metane.corp@gmail.com>
2014-11-13 19:21:59 +0100

    Final update repo

---
+++ TestSoundCloud/SoundCloudAPI/Track.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSoundCloud
+{
+    public class Track
+    {
+        public int? id { get; set; }
+        public string created_at { get; set; }
+        public string user_id { get; set; }
+        public User user { get; set; }
+        public string title { get; set; }
+        public string permalink { get; set; }
+        public string uri { get; set; }
+        public string sharing { get; set; }
+        public string embeddable_url { get; set; }
+        public string purchase_url { get; set; }
+        public string artwork_url { get; set; }
+        public string description { get; set; }
+        public User label { get; set; }
+        public int? duration { get; set; }
+        public string genre { get; set; }
+        public int? shared_to_count { get; set; }
+        public string tag_list { get; set; }
+        public int? label_id { get; set; }
+        public string label_name { get; set; }
+        public string release { get; set; }
+        public int? release_day { get; set; }
+        public int? release_month { get; set; }
+        public int? release_year { get; set; }
+        public bool streamable { get; set; }
+        public bool downloadable { get; set; }
+        public string state { get; set; }
+        public string license { get; set; }
+        public string track_type { get; set; }
+        public string waveform_url { get; set; }
+        public string download_url { get; set; }
+        public string stream_url { get; set; }
+        public string video_url { get; set; }
+        public float? bpm { get; set; }
+        public bool commentable { get; set; }
+        public string iscr { get; set; }
+        public string key_signature { get; set; }
+        public int? comment_count { get; set; }
+        public int? download_count { get; set; }
+        public int? playback_count { get; set; }
+        public int? favoritings_count { get; set; }
+        public string original_format { get; set; }
+        public int? original_content_size { get; set; }
+        public bool user_favorite { get; set; }
+    }
+}
---
+++ TestSoundCloud/UserControl/UserControlTrackPreview.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestSoundCloud
+{
+    /// <summary>
+    /// Logique d'interaction pour UserControlTrackPreview.xaml
+    /// </summary>
+    public partial class UserControlTrackPreview : UserControl
+    {
+        public UserControlTrackPreview()
+        {
+            InitializeComponent();
+        }
+
+        public void update(Track track)
+        {
+            labelTitle.Content = track.title;
+            labelAuthor.Content = track.user.username;
+
+            if (track.artwork_url != null)
+            {
+                BitmapImage img = new BitmapImage();
+                img.BeginInit();
+                img.UriSource = new Uri(track.artwork_url, UriKind.Absolute);
+                img.EndInit();
+                image.Source = img;
+            }
+
+            if (track.waveform_url != null)
+            {
+                BitmapImage img = new BitmapImage();
+                img.BeginInit();
+                img.UriSource = new Uri(track.waveform_url, UriKind.Absolute);
+                img.EndInit();
+                imageSound.Source = img;
+            }
+
+            labelStartedOn.Content = track.release;
+            labelPlays.Content = track.playback_count;
+            labelLikes.Content = track.favoritings_count.ToString();
+        }
+    }
+}
