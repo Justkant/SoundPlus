52423500475d176dcc7e1db7e1ae0b24bb0e01b6
MetaCorp <metane.corp@gmail.com>
2014-11-14 06:04:36 +0100

    Added list class, added lists to user preview

--- TestSoundCloud/UserControl/Preview/UserControlUserPreview.xaml.cs
+++ TestSoundCloud/UserControl/Preview/UserControlUserPreview.xaml.cs
@@ -19,9 +19,42 @@ namespace TestSoundCloud
     /// </summary>
     public partial class UserControlUserPreview : UserControl
     {
-        public UserControlUserPreview(User user, Window parent)
+        MainWindow parent;
+        User user;
+
+        public UserControlUserPreview(User user, MainWindow parent)
         {
             InitializeComponent();
+
+            this.parent = parent;
+            this.user = user;
+
+            if (user.avatar_url != null)
+            {
+                BitmapImage img = new BitmapImage();
+                img.BeginInit();
+                img.UriSource = new Uri(user.avatar_url, UriKind.Absolute);
+                img.EndInit();
+                imageAvatar.Source = img;
+            }
+
+            labelPseudo.Content = user.username;
+            labelLocation.Content = user.country;
+            labelFollowers.Content = user.followers_count;
+            labelSounds.Content = user.track_count;
+            textBlockDescription.Text = user.description;
+
+            UserControlListItem listTracks = new UserControlListItem("Tracks", parent.listBoxDownload);
+            UserControlListItem listPlaylists = new UserControlListItem("Playlists", parent.listBoxDownload);
+            UserControlListItem listFollowers = new UserControlListItem("Followers", parent.listBoxDownload);
+
+            //appeler la fct fill pour chaque list
+
+            Grid.SetColumn(listTracks, 0);
+            gridContent.Children.Add(listTracks);
+
+            stackPanelContent.Children.Add(listPlaylists);
+            stackPanelContent.Children.Add(listFollowers);
         }
     }
 }
---
+++ TestSoundCloud/UserControl/UserControlListItem.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestSoundCloud
+{
+    /// <summary>
+    /// Interaction logic for UserControlListItem.xaml
+    /// </summary>
+    public partial class UserControlListItem : UserControl
+    {
+        ListBox listBoxDownload;
+
+        public UserControlListItem(String title, ListBox listBoxDownload)
+        {
+            InitializeComponent();
+
+            this.listBoxDownload = listBoxDownload;
+
+            this.labelTitle.Content = title;
+        }
+
+        public void Fill(List<Track> tracks)
+        {
+            this.labelTitle.Content = tracks.Count + " " + this.labelTitle.Content;
+            foreach (Track track in tracks)
+                listBoxItems.Items.Add(new UserControlTrack(track, listBoxDownload));
+        }
+
+        public void Fill(List<Playlist> playlists)
+        {
+            this.labelTitle.Content = playlists.Count + " " + this.labelTitle.Content;
+            foreach (Playlist playlist in playlists)
+                listBoxItems.Items.Add(new UserControlPlaylist(playlist, listBoxDownload));
+        }
+
+        public void Fill(List<User> followers)
+        {
+            this.labelTitle.Content = followers.Count + " " + this.labelTitle.Content;
+            foreach (User user in followers)
+                listBoxItems.Items.Add(new UserControlUser(user, listBoxDownload));
+        }
+    }
+}
